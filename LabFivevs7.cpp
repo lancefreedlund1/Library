fd4707e3b621e83b24e9f0b0d618eec59fe7e97f
FlorinPost <33331086+FlorinPost@users.noreply.github.com>
2017-11-04 11:46:25 -0700

    Add files via upload

---
+++ LabFivevs7.cpp
@@ -0,0 +1,320 @@
+
+/*******************************
+CSC100AB 39013
+Assignments
+Lab 5 - Arrays
+
+Florin Postelnicu
+
+11/03/2017
+********************************/
+
+
+
+
+#include<time.h>
+#include <windows.h>
+#include <stdio.h>
+#include<fstream>
+#include<cstdlib>
+#include<iostream>
+
+
+
+const int array_size = 10;
+
+void presentation();//It presents the menu
+
+void fill_ups(int a[], int size);//It generates an array of int between 0-99
+
+
+void display_array(int a[], int size);//It displays an array
+
+
+void square_array(int a[], int  b[], int size);//It squares evry element of an array
+
+
+void replace_last(int a[], int b[], int size, int ldg);//It replaces the last digit
+
+
+void shift_right(int a[], int b[], int size);//It shifts right the contents of an array
+
+
+int count_multiples(int a[], int size, int div);//It counts multiples of a div(isor) in an array
+
+
+
+
+
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	using namespace std;
+	int choice, last_dg;
+	int array[array_size];
+	int barray[array_size];
+	int carray[array_size];
+	fill_ups(array, array_size);
+	char more;
+
+
+
+
+	do
+	{
+		cout << "List of options \n" << endl;
+
+		cout << " Enter a choice  \n" << endl;
+		presentation();
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+		{
+			//call display array
+
+
+			display_array(array, array_size);
+
+
+
+
+			break;
+		}//end case 1
+		case 2:
+		{
+			//call square_array
+
+			square_array(array, barray, array_size);
+			//display_array(array, 10);
+			//cout << endl;
+
+
+			break;
+		}//end case 2
+
+		case 3:
+		{
+			//call replace
+			cout << "Enter the last digit ldg = ";
+			cin >> last_dg;
+			if((last_dg>=0)&&(last_dg<=9))
+			{
+				replace_last(array, barray, array_size, last_dg);
+			}
+			else
+			{
+			cout<<"You are supposed to enter a DIGIT between 0-9"<<endl;
+		    }
+
+
+
+
+			break;
+		}//end case 3
+
+		case 4:
+		{
+			display_array(array,array_size);
+			for( int i = 0;i<=array_size -1;i++)
+			{
+				carray[i] =array[i];
+			}
+
+			//call shift_right
+
+            do
+            {
+
+            shift_right(carray, barray, array_size);
+            cout<<"Do you want to continue to shift (Y/N) ?";
+            cin>>more;
+            if(more!='y')
+            {
+				break;}
+				else
+				{
+			shift_right(barray, carray, array_size);
+
+			cout<<"Do you want to continue to shift (Y/N) ?";
+            cin>>more;
+		    }
+		}while( more =='y');
+
+
+			break;
+		}//end case 4
+
+		case 5:
+		{
+			int div;
+			//call count_multiples
+			cout << "\n "
+				<< "Will check how many terms of the array are multiples \n"
+				<< " of tha number  div \n" << endl;
+			cout <<" enter div = ";
+			cin >> div;
+			cout << endl;
+			cout << "The numbers of multiples of number " << div << " is :" << count_multiples(array, array_size, div);
+			cout << endl << endl;
+
+
+			break;
+		}//end case 5
+
+		case 6:
+		{
+			break;
+		}//end Quit
+
+
+		default:
+		{
+			cout << "You need to enter a positive integer, 1 though 6" << endl;
+			break;
+		}//end default
+
+        cout<<"Enter another choice :";
+
+		cin >> choice;
+		cout<<endl;
+		}//end switch
+
+	} while (choice != 6);
+
+
+	cout << "Thank you";
+
+
+
+
+	cin.get(); cin.get();
+	return 0;
+}//end main
+
+
+
+
+//Presentation
+
+void presentation()
+{
+	using namespace std;
+	cout << "In this Program you can choose between 6 options: \n\n "
+		<<"1 : Display the content of an array \n"
+		<<" 2 : Square each element of the  array \n"
+		<<" 3 : Replace last digit with a given ldg between 0-9 \n"
+		<<" 4 : Shift all contents of the aray one element to the right \n"
+		<<" 5 : Choose a  number div ,and count all its multiples among the elements of the array \n"
+		<<" 6 : Quit \n";
+}
+
+void fill_ups(int a[], int size)//Generate the array
+{
+	for (int i = 0; i <= size - 1; i++)
+	{
+		a[i] = rand() % 100;
+	}
+
+}
+
+void display_array(int a[], int size)// display an array
+{
+	using namespace std;
+
+
+	cout << " [ ";
+	int i;
+	for (i = 0; i < size - 1; i++)
+	{
+		cout << a[i] << ",";
+	}
+	cout << a[i] << " ] \n" << endl;
+
+}
+
+
+void square_array(int a[], int  b[], int size)
+{
+	using namespace std;
+
+
+
+	for (int i = 0; i <= size - 1; i++)
+	{
+		b[i] = a[i] * a[i];
+	}
+	display_array(a, size);
+	cout << endl;
+	display_array(b, size);
+	cout << endl;
+
+}
+
+void replace_last(int a[], int b[], int size, int  ldg)
+{
+	using namespace std;
+	display_array(a, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		if ((a[i] -ldg)%10 == 0)
+		{
+			b[i] = a[i];
+		}
+		else
+		{
+			b[i] = (a[i] / 10) * 10 + ldg;
+		}
+	}
+
+	cout << endl << endl;
+	display_array(b, size);
+	cout << "last digit is :" << ldg << endl;
+
+
+
+
+
+}
+
+
+void shift_right(int a[], int b[], int size)
+{
+	using namespace std;
+	int i;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		b[i] = a[i + 1];
+	}
+	b[i] = a[0];
+	//display_array(a, size);
+	cout << endl;
+	display_array(b, size);
+	cout << endl;
+
+
+}
+
+
+int count_multiples(int a[], int size, int div)
+{
+
+	display_array(a,size);
+
+	int counts = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (a[i] % div == 0)
+		{
+			counts = counts + 1;
+		}
+	}
+	return counts;
+
+}
